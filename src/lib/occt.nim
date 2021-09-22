0ae26ffd4c20d612b753878a4cc498a10b206203
mantielero <josemaria.alkala@gmail.com>
2021-09-22 23:28:33 +0200

    cinterop basic example

---
+++ src/lib/occt.nim
@@ -0,0 +1,7 @@
+{.passL: "-lTKBO -lTKSTEP -lTKPrim -lTKSTEPAttr -lTKSTEP209 -lTKSTEPBase -lTKXSBase -lTKShHealing -lTKTopAlgo -lTKGeomAlgo -lTKBRep -lTKGeomBase -lTKG3d -lTKG2d -lTKMath -lTKernel".}
+{.passC:"-I/usr/include/opencascade" .}
+
+#const libPath = "/usr/include/opencascade"
+
+import gp, gc
+export gp, gc

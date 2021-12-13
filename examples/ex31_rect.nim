31ed18bb2cae3b1a72534c3d180138dd8083c757
mantielero <josemaria.alkala@gmail.com>
2021-12-13 20:21:47 +0100

    Little improvement on the example

--- examples/ex31_rect.nim
+++ examples/ex31_rect.nim
@@ -1,8 +1,12 @@
 # Create a rectangle (that would be a wire based on 4 edges)
 import occt
 
+proc newSegment(p1,p2:Pnt):BRepBuilderAPI_MakeEdge =
+  var l1:HandleGeomTrimmedCurve = segment(p1, p2)
+  edge(l1)
 
-proc toSTEP[T:TopoDS_Solid| TopoDS_Wire ](fname:string; solid:T) =
+
+proc toSTEP[T:TopoDS_Solid | TopoDS_Wire ](fname:string; solid:T) =
   # Writer
   var writer = newSTEPControlWriter() 
   var tmp1 = setCVal("xstep.cascade.unit", "MM")
@@ -21,23 +25,19 @@ proc toSTEP[T:TopoDS_Solid| TopoDS_Wire ](fname:string; solid:T) =
 
 proc rect(p1,p2,p3,p4:Pnt):BRepBuilderAPI_MakeWire =
   # https://dev.opencascade.org/doc/refman/html/class_g_c___make_segment.html
-  var l1:HandleGeomTrimmedCurve = segment(p1, p2)
-  var l2:HandleGeomTrimmedCurve = segment(p2, p3)
-  var l3:HandleGeomTrimmedCurve = segment(p3, p4)
-  var l4:HandleGeomTrimmedCurve = segment(p4, p1)
-
-  var ll1:BRepBuilderAPI_MakeEdge = edge(l1)
-  var ll2:BRepBuilderAPI_MakeEdge = edge(l2)
-  var ll3:BRepBuilderAPI_MakeEdge = edge(l3)
-  var ll4:BRepBuilderAPI_MakeEdge = edge(l4)
+  var l1 = newSegment(p1, p2)
+  var l2 = newSegment(p2, p3)
+  var l3 = newSegment(p3, p4)
+  var l4 = newSegment(p4, p1)
 
   var w = wire()
-  w.add(ll1)
-  w.add(ll2)
-  w.add(ll3)
-  w.add(ll4)
+  w.add(l1)
+  w.add(l2)
+  w.add(l3)
+  w.add(l4)
   return w
 
+#-----
 var p1 = newPnt(0,0,0)
 var p2 = newPnt(0,20,0)
 var p3 = newPnt(30,20,0)

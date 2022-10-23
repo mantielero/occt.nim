e076e1367709ad9562f22535fae1b19cef74b036
mantielero <josemaria.alkala@gmail.com>
2022-10-23 14:20:31 +0200

    WiP

--- src/wrapper/postprocess.nim
+++ src/wrapper/postprocess.nim
@@ -1,4 +1,4 @@
-import std/[os, strutils, strformat]
+import std/[os, strutils, strformat, sequtils]#, parseutils]
 import system/io
 # Once we have the headers, we will modify them
 
@@ -51,7 +51,7 @@ proc getTypeBlock*(fname:string):tuple[typs:seq[TypObj], txt:string] =
     else:
       newFile &= line & "\n"
     #echo indent, " - ", currentIndent, " - ", (currentIndent < indent), ": ", line     
-    if currentIndent <= indent and line.strip != "type":
+    if currentIndent <= indent and line.strip != "type" and line.strip.len > 0:
       isType = false
       if typ.txt != @[]:
         typs &= typ
@@ -59,31 +59,41 @@ proc getTypeBlock*(fname:string):tuple[typs:seq[TypObj], txt:string] =
       typ.push = @[]
   return (typs, newFile)
 
-proc createTypesFile*(prefix:string) =
+proc createTypesFile*(pattern:string) =
   var txt = ""
-  for fname in walkFiles(fmt"./{prefix}*.nim"):
-    #if fname != fmt"./{prefix}types.nim":
-      var (typs, newFile) = getTypeBlock(fname)     
-      for typ in typs:
-        for p in typ.push:
-          txt &= p & "\n"
-        for line in typ.txt:
-          txt &= line & "\n"
-        for p in typ.push:
-          txt &= "{.pop.}\n"
 
-      if typs.len > 0:  # Si el fichero no contiene tipos, no tenemos que hacer nada con él
-        # Backup the existing file if we modify it
-        fname.copyFile(fname & ".backup")  # Backup old files
 
-        # Create the new file content
-        newFile = fmt"import {prefix}types" & "\n\n" & newFile
-        #for line in typ.newFile:
-        #  newFile &= line & "\n"
-        #echo newFile
-        fname.writeFile(newFile) 
-      
-  fmt"{prefix}types.nim".writeFile(txt)
+  var dirs = toSeq( walkDirs(pattern) )
+  for d in dirs:
+    var tmp = d.split('/')
+    var prefix = tmp[tmp.high]
+  #for fname in walkFiles("./tk*/*/*.nim"): 
+
+    for fname in walkFiles(fmt"{d}/{prefix}*.nim"):
+      #if fname != fmt"./{prefix}types.nim":
+        var (typs, newFile) = getTypeBlock(fname)     
+        for typ in typs:
+          for p in typ.push:
+            txt &= p & "\n"
+          for line in typ.txt:
+            txt &= line & "\n"
+          for p in typ.push:
+            txt &= "{.pop.}\n"
+
+        if typs.len > 0:  # Si el fichero no contiene tipos, no tenemos que hacer nada con él
+          # Backup the existing file if we modify it
+          fname.copyFile(fname & ".backup")  # Backup old files
+
+          # Create the new file content
+          newFile = fmt"import {prefix}_types" & "\n\n" & newFile
+          #for line in typ.newFile:
+          #  newFile &= line & "\n"
+          #echo newFile
+
+
+          fname.writeFile(newFile) 
+    #echo txt   
+    fmt"{d}/{prefix}_types.nim".writeFile(txt)
 
 # ---------------
 # 2. Rename types
@@ -125,6 +135,68 @@ proc renameProcs*(replaceProcs:seq[tuple[a,b:string]]) =
 
     fname.writeFile(txt)
 
+# ................................
+# 4. Replace includes with imports
+# --------------------------------
+proc convertIncludesToImports*(fname:string) = #replaceProcs:seq[tuple[a,b:string]]) =    
+  var txt = fname.readFile()
+  var lines = txt.splitLines()
+  var newlines:seq[string]
+  var includes:seq[string]
+  for line in lines:  
+    if line.strip.startsWith("include "):
+      var tmp = line.strip.split("include ")[1].replace(".", "/")
+      var tmp2 = tmp.split('/')
+      includes &= tmp2[tmp2.high]
+      newlines &= line.replace("include ", "import ")
+    else:
+      newlines &= line
+  # We only modify the file if it contains some "include" statement
+  if includes.len > 0:
+    newlines &= "export"
+    for incl in includes:
+      newlines[newlines.high] &= " " & incl & ","
+    var tmp = newlines[newlines.high] 
+    newlines[newlines.high] = tmp[(tmp.low) .. (tmp.high-1)]
+    var newtxt = ""
+    for line in newlines:
+      newtxt &= line & "\n"
+  
+    fname.writeFile(newtxt)
+
+
+proc convertIncludesToImports*() = #replaceProcs:seq[tuple[a,b:string]]) =
+  for fname in walkFiles("./tk*/*/*.nim"): 
+    convertIncludesToImports(fname)
+  for fname in walkFiles("./tk*/*.nim"): 
+    convertIncludesToImports(fname)    
+
+
+# ------------------------------------------------
+# 5. Convert discarded lines into something useful
+# ------------------------------------------------
+
+#
+# 9. Check includes contains the _types files
+#
+proc correctIncludes(pattern:string) =
+  var txt = ""
+
+  var dirs = toSeq( walkDirs(pattern) )
+  for d in dirs:
+    var tmp = d.split('/')
+    var prefix = tmp[tmp.high]
+
+  #for fname in walkFiles("./tk*/*/*.nim"): 
+    var fname = fmt"{d}/{prefix}_includes.nim"
+    if fname.fileExists:
+      txt = fname.readFile()
+      
+      if not txt.contains(fmt"{prefix}_types"):
+        txt &= fmt"import {prefix}_types" & "\n" & fmt"export {prefix}_types" & "\n"
+      fname.writeFile(txt)
+      
+
 # -----------------------
 # 10. Append to beginning
 # -----------------------
@@ -137,9 +209,13 @@ proc appendBeg*(fname, append:string) =
 #===================================================
 #===================================================
 
-# 1. 
-#createTypesFile("gp_")
+# 1. Create types with files
 
+#"./tkmath/gp"
+#createTypesFile("./tk*/*")
+
+createTypesFile("./tkernel/standard")
+#correctIncludes("./tk*/*")
 
 # 2. Rename types
 # thmath/gp
@@ -149,28 +225,30 @@ var tmp = @["Ax1", "Ax2", "Ax2d", "Ax3", "Ax22d", "Circ", "Circ2d", "Cone", "Cyl
   "Lin", "Lin2d", "Mat", "Mat2d", "Parab", "Parab2d", "Pln", "Pnt", "Pnt2d", "Quaternion",
   "QuaternionSlerp", "Sphere", "Torus", "Trsf", "Trsf2d", "TrsfForm", "Vec", "Vec2d",
   "VectorWithNullmag", "Xy", "Xyz" ]
-for i in tmp:
-  replaceTypes &= (i,  i & "Obj")  
+
+#for i in tmp:
+#  replaceTypes &= (i,  i & "Obj")  
 
 
 
 
-renameTypes(replaceTypes)
+#renameTypes(replaceTypes)
 
 # 3. Rename procs
 var replaceProcs: seq[tuple[a,b:string]]
-for i in tmp:
-  replaceProcs &= ("new" & i, i.toLowerAscii)
+#for i in tmp:
+#  replaceProcs &= ("new" & i, i.toLowerAscii)
 
 
-renameProcs(replaceProcs)
+#renameProcs(replaceProcs)
 
 
 # ......
 # 10. Other mods
 #------
-#var txt = "tkmath/gp/gp_types.nim".readFile
-"tkmath/gp/gp_types.nim".appendBeg("import tkernel/standard/[standard_handle]")
-#for fname in walkDirs("./tk*"):
-#for fname in walkFiles("./tk*/*/*.nim"):  
-#  echo fname
+
+#"tkmath/gp/gp_types.nim".appendBeg("import tkernel/standard/[standard_handle]")
+
+
+
+#convertIncludesToImports()

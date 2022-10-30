31de709ff8063156383b2e5a28307f394b33441b
mantielero <josemaria.alkala@gmail.com>
2022-10-30 10:19:37 +0100

    includes to import

--- src/wrapper/occt_wrapper.nim
+++ src/wrapper/occt_wrapper.nim
@@ -1,21 +1,21 @@
-{.experimental: "codeReordering".}
+#{.experimental: "codeReordering".}
 {.experimental: "callOperator".}
 
 proc cnew*[T](x: T): ptr T {.importcpp: "(new '*0#@)", nodecl.}
 
-include tkernel/tkernel
-include tkmath/tkmath
-include tkgeombase/tkgeombase
-include tkg3d/tkg3d
-include tkg2d/tkg2d
-include tkbrep/tkbrep
-include tktopalgo/tktopalgo
-include tkprim/tkprim
-include tkfillet/tkfillet
-include tkbool/tkbool
-include tkgeomalgo/tkgeomalgo
-include tkbo/tkbo
-include tkxsbase/tkxsbase
-include tkstep/tkstep
-include tkv3d/tkv3d
-include tkservice/tkservice
+import tkernel/tkernel
+import tkmath/tkmath
+import tkgeombase/tkgeombase
+import tkg3d/tkg3d
+import tkg2d/tkg2d
+import tkbrep/tkbrep
+import tktopalgo/tktopalgo
+import tkprim/tkprim
+import tkfillet/tkfillet
+import tkbool/tkbool
+import tkgeomalgo/tkgeomalgo
+import tkbo/tkbo
+import tkxsbase/tkxsbase
+import tkstep/tkstep
+import tkv3d/tkv3d
+import tkservice/tkservice
